dad02c4ac4a423ff733ddec074c980cc6cedd0ec
yottacto <yotta.w.cto@gmail.com>
2018-06-23 00:49:50 +0800

    + batch split worker for sync-paramter-server

--- src/sync-paramter-server.py
+++ src/sync-paramter-server.py
@@ -11,6 +11,7 @@ import time
 
 parser = argparse.ArgumentParser(description="Run the synchronous parameter server example.")
 parser.add_argument("--num-workers", default=4, type=int, help="The number of workers to use.")
+parser.add_argument("--batch-size", default=64, type=int, help="Batch size.")
 parser.add_argument("--redis-address", default=None, type=str, help="The Redis address of the cluster.")
 
 
@@ -29,15 +30,37 @@ class ParameterServer(object):
 
 @ray.remote
 class Worker(object):
-    def __init__(self, worker_index, batch_size=50):
+    def __init__(self, worker_index, num_workers, batch_size=64):
         self.worker_index = worker_index
-        self.batch_size = batch_size
-        self.ds = model.load_data()
-        self.net = model.simple()
+        self.num_workers  = num_workers
+        self.batch_size   = batch_size
+        self.block_size   = batch_size // num_workers
+        self.ds           = model.load_data()
+        self.net          = model.simple()
 
     def compute_gradients(self, weights):
         self.net.variables.set_flat(weights)
         xs, ys = self.ds.train.next_batch(self.batch_size)
+        return self.net.compute_gradients(xs, ys)
+
+
+@ray.remote
+class SplitBatchWorker(object):
+    def __init__(self, worker_index, num_workers, batch_size=64):
+        self.worker_index = worker_index
+        self.num_workers  = num_workers
+        self.batch_size   = batch_size
+        self.block_size   = batch_size // num_workers
+        self.start        = self.worker_index * self.block_size
+        self.end          = self.batch_size if self.worker_index == self.num_workers - 1 else self.start + self.block_size
+        self.ds           = model.load_data()
+        self.net          = model.simple()
+
+    def compute_gradients(self, weights):
+        self.net.variables.set_flat(weights)
+        xs, ys = self.ds.train.next_batch(self.batch_size)
+        xs = xs[self.start : self.end]
+        ys = ys[self.start : self.end]
         return self.net.compute_gradients(xs, ys)
 
 
@@ -51,8 +74,9 @@ if __name__ == "__main__":
     ps = ParameterServer.remote(1e-4 * args.num_workers)
 
     # Create workers.
-    workers = [Worker.remote(worker_index)
-               for worker_index in range(args.num_workers)]
+    batch_size = args.batch_size
+    workers = [SplitBatchWorker.remote(worker_index, args.num_workers, batch_size)
+            for worker_index in range(args.num_workers)]
 
     # Download Data.
     ds = model.load_data()
@@ -61,19 +85,21 @@ if __name__ == "__main__":
     current_weights = ps.get_weights.remote()
     tot_start = time.time()
     iteration_start = tot_start
-    while i < 300:
+    epoch = 0
+    while epoch < 10:
         # Compute and apply gradients.
         gradients = [worker.compute_gradients.remote(current_weights)
-                     for worker in workers]
+                for worker in workers]
         current_weights = ps.apply_gradients.remote(*gradients)
 
-        if i % 10 == 0:
+        if epoch != i * batch_size // ds.train.num_examples:
             # Evaluate the current model.
             net.variables.set_flat(ray.get(current_weights))
             test_xs, test_ys = ds.test.next_batch(ds.test.num_examples)
             accuracy = net.compute_accuracy(test_xs, test_ys)
-            print("Iteration {}: accuracy is {}, time is {}s".format(i, accuracy, time.time() - iteration_start))
+            print("Epoch {}: accuracy is {}, time is {}s".format(epoch, accuracy, time.time() - iteration_start))
             iteration_start = time.time()
+            epoch = i * batch_size // ds.train.num_examples
 
         i += 1
 
