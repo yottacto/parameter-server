528528671cf6aed50be7fb7b50753d4ad425a67b
yottacto <yotta.w.cto@gmail.com>
2018-06-23 00:49:38 +0800

    @ model, tf.float32

--- src/model.py
+++ src/model.py
@@ -52,7 +52,7 @@ class simple(object):
             self.grads = self.optimizer.compute_gradients(
                 self.cross_entropy)
             self.grads_placeholder = [
-                (tf.placeholder("float", shape=grad[1].get_shape()), grad[1])
+                (tf.placeholder(tf.float32, shape=grad[1].get_shape()), grad[1])
                 for grad in self.grads]
             self.apply_grads_placeholder = self.optimizer.apply_gradients(
                 self.grads_placeholder)
